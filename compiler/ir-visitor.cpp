c9ae821ed081622db15b5fc7f3245248034301e0
FayalaMohamed <fayalla.mohamed@gmail.com>
2024-03-13 09:19:39 +0100

    added const visitor

--- compiler/ir-visitor.cpp
+++ compiler/ir-visitor.cpp
@@ -2,6 +2,7 @@
 #include "ir/ir-cfg.h"
 #include "ir/ir-basic-block.h"
 #include "ir/instr/assign.h"
+#include "ir/instr/expression_cst.h"
 
 antlrcpp::Any IRVisitor::visitProg(ifccParser::ProgContext *ctx)
 {
@@ -14,5 +15,10 @@ antlrcpp::Any IRVisitor::visitAffectation(ifccParser::AffectationContext *ctx)
     this->visit(ctx->expr());
     cfg->get_current_bb()->add_instr(new IR::IRInstrAssign(cfg->get_current_bb(), ctx->VAR()->getText()));
     
+    return 0;
+}
+
+antlrcpp::Any IRVisitor::visitExprConst(ifccParser::ExprConstContext *ctx){
+    cfg->get_current_bb()->add_instr(new IR::IRInstrExprCst(cfg->get_current_bb(), ctx->CONST()->getText()));
     return 0;
 }
