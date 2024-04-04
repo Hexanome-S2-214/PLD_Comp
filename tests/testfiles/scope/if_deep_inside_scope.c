e31a634c6063ff65729f5da20c6b32c89b3be694
Charles-François St-Cyr <charlesf2001@gmail.com>
2024-04-04 20:29:44 +0200

    add test

--- tests/testfiles/scope/if_deep_inside_scope.c
+++ tests/testfiles/scope/if_deep_inside_scope.c
@@ -27,34 +27,3 @@ int main() {
 
     return a;
 }
-
-/*
-int main() {
-    int a = 1;
-
-    if (1) {
-        int b = 2;
-
-        if (1) {
-            int c = 3;
-        return 10;
-
-            if (1) {
-                int d = 4;
-
-                if (1) {
-                    int e = 5;
-
-                    if (1) {
-                        a = a + b + c + d + e;
-
-                        return 100;
-                    }
-                }
-            }
-        }
-    }
-
-    return a;
-}
-*/
