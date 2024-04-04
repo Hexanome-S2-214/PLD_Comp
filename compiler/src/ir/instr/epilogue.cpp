56faeee76b323a549361ef0a5462b3502cbf03d6
FayalaMohamed <fayalla.mohamed@gmail.com>
2024-04-04 11:59:09 +0200

    ret

--- compiler/src/ir/instr/epilogue.cpp
+++ compiler/src/ir/instr/epilogue.cpp
@@ -12,6 +12,7 @@ namespace IR
     void IRInstrEpilogue::gen_asm_arm(ostream& o)
     {
         o << "\tadd	sp, sp, 16\n";
+        o << "\tret\n";
         //o << "\tpop {fp, pc}\n";
 
         //o << "\t\tldp x29, x30, [sp], #16\n"; 
