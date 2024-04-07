1a4a08e9f0392f64d45d539b73c108608143502e
gagathiou <agathe.autier@insa-lyon.fr>
2024-04-07 10:55:35 +0200

    some modifications after merge w main

--- tests/testfiles/table/tableChar.c
+++
@@ -1,6 +0,0 @@
-int main(){
-    char foo[3];
-    foo[2] = 'a';
-    foo[3] = foo[2] + 1;
-    return foo[3];
-}
--- tests/testfiles/table/tableReturnIndex.c
+++
@@ -1,6 +0,0 @@
-int main() {
-  int foo[5];
-  foo[3] = 10;
-  int a = foo[3];
-  return a;
-}
